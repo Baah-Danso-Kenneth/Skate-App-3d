c1c5d6e6fc6818f0990990844e5ac2b6d1b89407
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-18 14:17:56 +0000

    Logically create Sqiggle lines

--- app/components/pages/homePage/SkateBoardContent.tsx
+++ app/components/pages/homePage/SkateBoardContent.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { skateBoards } from '@/app/data';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaStar } from 'react-icons/fa6';
 import { ButtonLink } from '../../ButtonLink';
 import { HorizontalLine, VerticalLine } from '../../Lines';
 import clsx from 'clsx';
+import { Scribble } from '../../Squibble';
 
 type Props = {
   item: {
@@ -17,19 +18,48 @@ type Props = {
   };
 };
 
+
+
+async function getDominantColor(relativeUrl: string) {
+  
+    const baseUrl = typeof window !== "undefined" ? window.location.origin : "";
+    const palleteURL = new URL(`${baseUrl}${relativeUrl}`);
+    palleteURL.searchParams.set("palette", "json");
+  
+    const res = await fetch(palleteURL.toString());
+    const json = await res.json();
+  
+    return (
+      json.dominant_colors?.vibrant?.hex || json.dominant_colors?.vibrant_light?.hex
+    );
+  }
+  
+
 const VERTICAL_LINE_CLASSES =
   "absolute top-0 h-full stroke-2 text-stone-300 transition-colors group-hover:text-stone-400";
 
 const HORIZONTAL_LINE_CLASSES =
   "-mx-8 stroke-2 text-stone-300 transition-colors group-hover:text-stone-400";
 
-
 function SkateBoardContent({ item }: Props) {
+  const [dominantColor, setDominantColor] = useState<string>();
+
+  useEffect(() => {
+    const fetchColor = async () => {
+      const color = await getDominantColor(item.image); 
+      setDominantColor(color);
+    };
+    console.log(fetchColor)
+
+    fetchColor();
+  }, [item.image]);
+
   return (
     <div className="relative group mx-auto w-full max-w-72 px-8 pt-2">
-        <VerticalLine className={clsx(VERTICAL_LINE_CLASSES,"left-4")}/>
-        <VerticalLine className={clsx(VERTICAL_LINE_CLASSES,"right-4")}/>
-        <HorizontalLine className={HORIZONTAL_LINE_CLASSES}/>
+      <VerticalLine className={clsx(VERTICAL_LINE_CLASSES, "left-4")} />
+      <VerticalLine className={clsx(VERTICAL_LINE_CLASSES, "right-4")} />
+      <HorizontalLine className={HORIZONTAL_LINE_CLASSES} />
+
       {/* Price and Rating */}
       <div className="flex items-center justify-between ~text-sm/2xl">
         <span>${item.price.toFixed(2)}</span>
@@ -40,7 +70,8 @@ function SkateBoardContent({ item }: Props) {
       </div>
 
       {/* Image */}
-      <div className="~mb-1 py-4 overflow-hidden flex justify-center">
+      <div className=" ~mb-1 py-4 overflow-hidden flex justify-center">
+        <Scribble className="absolute inset-0 w-full h-full" color={dominantColor || '#ccc'} />
         <Image
           className="mx-auto w-[58%] origin-top transform-gpu
             transition-transform duration-500
@@ -51,7 +82,7 @@ function SkateBoardContent({ item }: Props) {
           height={150}
         />
       </div>
-      <HorizontalLine className={HORIZONTAL_LINE_CLASSES}/>
+      <HorizontalLine className={HORIZONTAL_LINE_CLASSES} />
 
       {/* Name */}
       <div className="my-2 text-center font-sans leading-tight ~text-lg/xl">
@@ -62,12 +93,13 @@ function SkateBoardContent({ item }: Props) {
       <div
         className="absolute inset-0 z-10 flex items-center
           justify-center opacity-0 transition-opacity duration-200
-          group-hover:opacity-100 "
+          group-hover:opacity-100"
       >
         <ButtonLink href="#">Customize</ButtonLink>
       </div>
     </div>
   );
 }
+
 
 export default SkateBoardContent;
