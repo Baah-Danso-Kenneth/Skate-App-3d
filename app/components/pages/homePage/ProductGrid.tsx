ac917e2e26a375718a2cb87f7b26c4223ebb9d68
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-18 13:07:02 +0000

    Enhance logic for grabbing static data

--- app/components/pages/homePage/ProductGrid.tsx
+++ app/components/pages/homePage/ProductGrid.tsx
@@ -7,6 +7,8 @@ import { Bounded } from '../../shared/Bounded'
 import { Heading } from '../../Heading'
 import { skateBoards } from '@/app/data'
 import { FaStar } from 'react-icons/fa6'
+import { ButtonLink } from '../../ButtonLink'
+import SkateBoardContent from './SkateBoardContent'
 
 
 function ProductGrid() {
@@ -31,22 +33,11 @@ function ProductGrid() {
             Grab our freshest designs
         </div>
 
-        <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-        {skateBoards.map(({id,image,price,rating})=>(
-            <div key={id} className=''>
-                <div className='flex items-center justify-between ~text-sm/2xl'>
-                  <span className='font-mono'>${price.toFixed(2)}</span>
-                  <span className='inline-flex items-center gap-1 font-mono'>
-                    <FaStar className='text-yellow-400'/> {rating}
-                  </span>
-                </div>
-                <Image src={image} 
-                alt="image" 
-                className='w-full h-full'
-                width={300} height={300}/>
-            </div>
-        ))}
-        </div>
+         <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4'>
+          {skateBoards.map(item=>(
+            <SkateBoardContent item={item}/>
+          ))}
+         </div>
         
     </Bounded>
   )
