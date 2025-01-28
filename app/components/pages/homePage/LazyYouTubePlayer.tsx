e61c24fae15dcc093037f34240b0619f75f041ee
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-28 13:43:49 +0000

    Video Section UI section

--- app/components/pages/homePage/LazyYouTubePlayer.tsx
+++ app/components/pages/homePage/LazyYouTubePlayer.tsx
@@ -1,13 +1,40 @@
+"use client"
+
 import { KeyTextField } from '@prismicio/client'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 type Props = {
     youtubeID: KeyTextField
 }
 
 function LazyYouTubePlayer({youtubeID}: Props) {
+  const [isInView, setIsInView] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null)
+  
+  useEffect(()=>{
+    const currenctContainerRef = containerRef.current;
+
+    const observer = new IntersectionObserver(
+      ([entry])=>{
+        if(entry.isIntersecting){
+          setIsInView(true)
+        }
+      },{threshold:0, rootMargin: "1500px"}
+    );
+
+    if(currenctContainerRef){
+      observer.observe(currenctContainerRef)
+    }
+
+    return()=>{
+      if(currenctContainerRef){
+        observer.unobserve(currenctContainerRef);
+      }
+    }
+  })
+
     return (
-        <div className="relative h-full w-full">
+        <div ref={containerRef} className="relative h-full w-full">
           <iframe
             src={`https://www.youtube-nocookie.com/embed/${youtubeID}?autoplay=1&mute=1&loop=1&playlist=${youtubeID}`}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
--- app/components/pages/homePage/VideoSection.tsx
+++ app/components/pages/homePage/VideoSection.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Bounded } from '../../shared/Bounded'
 import LazyYouTubePlayer from './LazyYouTubePlayer'
+import clsx from 'clsx'
+import Image from 'next/image'
 
 const youtubeID='44I29krtxa'
+
+const MASK_CLASSES =
+  "[mask-image:url(/images/video-mask.png)] [mask-mode:alpha] [mask-position:center_center] [mask-repeat:no-repeat] [mask-size:100%_auto]";
 
 function VideoSection() {
   return (
@@ -10,7 +15,31 @@ function VideoSection() {
     <section>
         <h2 className='sr-only'>Video reel</h2>
         <div className="relative aspect-video">
+
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-brand-lime absolute inset-0 ~translate-x-2/3 ~translate-y-2/3"
+          )}
+        />
+
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/3 ~translate-y-1/2"
+          )}
+        />
+
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/2 --translate-y-1/3"
+          )}
+        />
+           <div className={clsx(MASK_CLASSES,"relative h-full ")}>
             <LazyYouTubePlayer youtubeID={youtubeID}/>
+            <Image src="/images/image-texture.png" alt="" fill className="pointer-events-none object-cover opacity-50"/>
+           </div>
         </div>
      </section>
     </Bounded>
